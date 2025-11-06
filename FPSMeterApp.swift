8b1aa2989dcd50d1f8e84a7f6da842a24a3e7896
Subhobhai <sarkarsubhadip604@gmail.com>
2025-11-06 22:50:16 +0530

    Add FPSMeterApp.swift - Main app entry point

---
+++ FPSMeterApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct FPSMeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
+    }
+}
